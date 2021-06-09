78b7f56b9bde656dd1f8e56d344ed27b315e34c3
RomanCianci <84516985+Roman-Cianci@users.noreply.github.com>
2021-06-09 13:31:43 +0000

    CHanged things

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-var age = 25
-if (age >= 17) {
-  alert("You can watch R rated movies alone.")
-} else if ( (age >= 13) ) {
-  alert("You can see a PG 13 movie alone.")
-} else {
-  alert("You can see a G rated movie.")
+// 
+document.getElementById('button').addEventListener('click', comparision)
+let age = 0
+
+function comparision() {
+    // get the age number and convert to an integer
+   age = document.getElementById('input').value
+   age = parseInt(age)
+
+// checks if user's age  is younger than 17
+  if (age >= 17) { 
+  document.getElementById('answer').innerHTML = 'You can see an R  movie alone'
+} else if (age >= 13) {
+  document.getElementById('answer').innerHTML = 'You can see a PG-13 movie alone'
+} else if (age >= 5) {
+  document.getElementById('answer').innerHTML = 'You can see a G or PG  movie alone'
+} else  {
+  document.getElementById ('answer').innerHTML = 'You're too young to watch movies'
 }
