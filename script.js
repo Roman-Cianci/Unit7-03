a51154f1797cdf30f045b142367fae9fc5226952
RomanCianci <84516985+Roman-Cianci@users.noreply.github.com>
2021-06-09 13:08:34 +0000

    Run again

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var age = 25
 if (age >= 17) {
-  write("You can watch R rated movies alone.");
+  alert("You can watch R rated movies alone.")
 } else if ( (age >= 13) ) {
-  write("You can see a PG 13 movie alone.");
+  alert("You can see a PG 13 movie alone.")
 } else {
-  write("You can see a G rated movie.");
+  alert("You can see a G rated movie.")
 }
